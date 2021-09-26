6e6db16f718481f27774e94bc4b225c2ee87aeb0
CDelannois <charles.delannois@live.be>
2021-09-26 13:05:22 +0200

    creation projet

---
+++ src/components/Cube.vue
@@ -0,0 +1,198 @@
+<template>
+  <b-row align-h="center">
+    <b-col class="test" cols="10">
+      <b-card title="Cube together" border-variant="primary" id="cube">
+        <p v-if="language === 'french'">Calculateur de qualité de résolution</p>
+        <p v-if="language === 'english'">Quality solve calculator</p>
+        <p v-if="language === 'french'">
+          Pensé par
+          <a
+            href="https://www.youtube.com/channel/UC04tRiZgDsyvGj3wcTNg7TA/featured"
+            target="_blank"
+            >DamarusRex</a
+          >
+          développé avec
+          <a
+            href="https://www.linkedin.com/in/charles-delannois-9416a6193/"
+            target="_blank"
+            >CDelannois</a
+          >
+        </p>
+        <p v-if="language === 'english'">
+          Designed by
+          <a
+            href="https://www.youtube.com/channel/UC04tRiZgDsyvGj3wcTNg7TA/featured"
+            target="_blank"
+            >DamarusRex</a
+          >
+          developped with
+          <a
+            href="https://www.linkedin.com/in/charles-delannois-9416a6193/"
+            target="_blank"
+            >CDelannois</a
+          >
+        </p>
+        <b-button
+          v-if="language === 'french'"
+          size="sm"
+          @click="switchToEnglish()"
+        >
+          English version
+        </b-button>
+        <b-button
+          v-if="language === 'english'"
+          size="sm"
+          @click="switchToFrench()"
+        >
+          Version française
+        </b-button>
+        <b-row align-h="center" class="mt-3">
+          <b-col cols="5">
+            <p v-if="language === 'french'">Moyenne</p>
+            <p v-if="language === 'english'">Average</p>
+          </b-col>
+          <b-col cols="5" id="ecart_type">
+            <p v-if="language === 'french'">Ecart-Type</p>
+            <p v-if="language === 'english'">Std. Deviation</p>
+            <b-tooltip
+              v-if="language === 'french'"
+              target="ecart_type"
+              triggers="hover"
+            >
+              Message ici
+            </b-tooltip>
+            <b-tooltip
+              v-if="language === 'english'"
+              target="ecart_type"
+              triggers="hover"
+            >
+              Message here
+            </b-tooltip>
+          </b-col>
+        </b-row>
+
+        <b-row align-h="center">
+          <b-col cols="5"
+            ><input
+              class="input"
+              type="text"
+              v-model="average"
+              @keyup="calculate()"
+            />
+          </b-col>
+          <b-col cols="5"
+            ><input
+              class="input"
+              type="text"
+              v-model="deviation"
+              @keyup="calculate()"
+            />
+          </b-col>
+        </b-row>
+
+        <div v-if="!valuesOK" class="mt-3">
+          <p v-if="language === 'french'">
+            Veuillez compléter les champs avec des valeurs en secondes
+          </p>
+          <p v-if="language === 'english'">
+            Please fill in the fields with values in seconds
+          </p>
+        </div>
+
+        <div v-else>
+          <div class="mt-3">
+            <p v-if="language === 'french'">Paramètres</p>
+            <p v-if="language === 'english'">Settings</p>
+          </div>
+          <b-row>
+            <b-col>
+              <p v-if="language === 'french'">Succès Critique</p>
+              <p v-if="language === 'english'">Critical Success</p>
+              <p><span class="fas fa-less-than"></span> {{ success }}</p>
+            </b-col>
+
+            <b-col>
+              <p v-if="language === 'french'">Succès</p>
+              <p v-if="language === 'english'">Success</p>
+              <p>Entre {{ success }} et {{ failure }}</p>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col>
+              <p v-if="language === 'french'">Échec</p>
+              <p v-if="language === 'english'">Failure</p>
+              <p>Entre {{ failure }} et {{ criticalFailure }}</p>
+            </b-col>
+
+            <b-col>
+              <p v-if="language === 'french'">Échec critique</p>
+              <p v-if="language === 'english'">Critical Failure</p>
+              <p><span class="fas fa-greater-than-equal"></span>{{ criticalFailure }}</p>
+            </b-col>
+          </b-row>
+        </div>
+
+        <p v-if="language === 'french'">Comment ça fonctionne?</p>
+        <p v-if="language === 'english'">How it all works?</p>
+      </b-card>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: "Cube",
+  props: {
+    msg: String,
+  },
+
+  data() {
+    return {
+      language: "french",
+      average: 0,
+      deviation: 0,
+      valuesOK: false,
+      success: "",
+      failure: "",
+      criticalFailure: "",
+    };
+  },
+
+  methods: {
+    switchToEnglish() {
+      this.language = "english";
+    },
+    switchToFrench() {
+      this.language = "french";
+    },
+    calculate() {
+      let averageNumber = parseInt(this.average);
+      let deviationNumber = parseInt(this.deviation);
+
+      if (
+        typeof averageNumber === "number" &&
+        averageNumber > 0 &&
+        typeof deviationNumber === "number" &&
+        deviationNumber > 0
+      ) {
+        this.valuesOK = true;
+        this.success = Math.round(averageNumber - deviationNumber);
+        this.failure = Math.round(averageNumber + deviationNumber);
+        this.criticalFailure = Math.round(averageNumber + deviationNumber * 3);
+      } else {
+        this.valuesOK = false;
+      }
+    },
+  },
+
+  computed: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input {
+  width: 40%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/regular.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
